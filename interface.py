a7213f0433af743ad69eb78c81e8e662dbf95a89
Wiktor <wpiela9@gmail.com>
2023-05-11 08:01:41 +0200

    Login class added, console interface extended

--- interface.py
+++ interface.py
@@ -1,25 +1,55 @@
-from modules.classes import Register
+from modules.classes import Register, Login
 import getpass
 
-decision = input("").lower()
+notification = """
+    Type 'register' to create new account.
+    Type 'login' to sing in.
+    Type 'exit' to quit.'
+"""
 
-while decision != "x":
+print(notification)
+decision = input("What do you wanna do? ").lower()
 
-    email = input("Type email: ")
-    password = getpass.getpass("Enter password: ")
-    confirmPassword = getpass.getpass("Confirm password: ")
+while decision != "exit":
 
-    if password==confirmPassword:
-        if Register.validate_email(email):
-            if not Register.email_taken(email):
-                newUser = Register(email, password)
-                newUser.save_new_user()
+    if decision == "register":
+        email = input("Type email: ")
+        password = getpass.getpass("Enter password: ")
+        confirmPassword = getpass.getpass("Confirm password: ")
+
+        if password==confirmPassword:
+            if Register.validate_email(email):
+                if not Register.email_taken(email)[0]:
+                    newUser = Register(email, password)
+                    newUser.save_new_user()
+                    print("Your account has been successfully created!")
+                    decision = input("Type 'login' to sing in: ").lower()
+                else:
+                    print("This email is already taken. Please ry again!")
+                    break
             else:
-                print("This email is already taken. Please ry again!")
+                print("Your email is not valid. Try again!")
                 break
         else:
-            print("Your email is not valid. Try again!")
+            print("Both passwords are not the same. Try again!")
+            break
+
+    elif decision=="login":
+        email = input("Type email: ")
+        if Login.email_taken(email):
+            password = getpass.getpass("Enter password: ")
+            newLogin = Login(email, password)
+            if newLogin.check_password_for_user(email, password):
+                print("You have been successfully logged in!")
+                decision = input("What do you wanna do next? ").lower()
+            else:
+                print("Wrong password. Try again!")
+                decision = input("What do you wanna do next? ").lower()
+        else:
+            print("This email doesn't exist. Registration required!")
             break
     else:
-        print("Both passwords are not the same. Try again!")
-        break
+        print(f"Command {decision} not found. Try again!")
+        decision = input("What do you wanna do next? ").lower()
+
+print("Bye!")
--- modules/classes.py
+++ modules/classes.py
@@ -1,12 +1,19 @@
 import json, re
 
 class Register:
+    """
+    This class creates new user object with email and password attributes.
+    Additionally: 
+        -checking email format correctness, doesn't allow to register 
+        -it doesn't allow to register more then one user with the same email addess
+        -saving new user object to json file if requirements are satisfied
+    """
     def __init__(self, email, password):
         self.__email = email
         self.__password = password
 
     @staticmethod
-    def validate_email(email):
+    def validate_email(email:str):
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         if re.fullmatch(regex, email):
             return True
@@ -14,7 +21,7 @@ class Register:
             return False
     
     @staticmethod
-    def email_taken(currentEmail):
+    def email_taken(currentEmail:str):
         with open("userdb.json", 'r') as openfile:
             json_object = json.load(openfile)
 
@@ -37,5 +44,24 @@ class Register:
             json.dump(json_object, feedsjson)
 
 class Login(Register):
-    def __init__(self, email, password):
-        super().__init__(email, password)
+    """
+    Register class but extended by method check_password_for_user for checking if 
+    user provided correct password.
+    """
+    def check_password_for_user(self, currentMail:str, inputPassword:str):
+
+        with open("userdb.json", 'r') as openfile:
+            json_object = json.load(openfile)
+
+        usersCount = len(json_object)
+
+        for x in range(usersCount):
+            if currentMail==json_object[x]["email"]:
+                whichUser = x
+                break
+
+        if json_object[whichUser]["password"] == inputPassword:
+            return True
+        else:
+            return False
+
